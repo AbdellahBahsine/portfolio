b27c9f995161277d87a7ca7eddefd9c9d9181ba9
paylo <abdellah.bahsine@hotmail.com>
2021-12-13 07:37:57 +0100

    changed text in about section

--- pages/components/about/about.component.jsx
+++ pages/components/about/about.component.jsx
@@ -5,9 +5,11 @@ const About = () => {
         <section id="about" className={styles.about}>
             <div className={styles.about__inner}>
                 <h2 className={styles.about__title}>About <span>Me</span></h2>
-                <p className={styles.about__text}>My name is Abdellah, I'm a front end developer based in Morocco. I'm passionate about coding and problem solving. I'm always trying to improve my programming skills by learning new stuff and be up to date.
-                <br /><br />I have experience working primarily with HTML, CSS, Sass, Boostrap, JavaScript and React JS, but I'm also learning new technologies and expanding my skillset to include the full-stack by learning MongoDB and NodeJS. My end goal is to become a fullstack and create fully functioning applications.
-                <br /><br />Other than coding, I love reading about philosophy because it helps challenge my thoughts/beliefs, playing or watching video games and listening to music.</p>
+                <p className={styles.about__text}>My name is Abdellah, I'm a front-end developer based in Morocco. I'm passionate about coding and problem-solving. I'm always trying to improve my programming skills by learning new stuff and being up to date.                
+                <br /><br />
+                I have experience working primarily with HTML, CSS, Sass, Bootstrap, JavaScript, and React JS, but I'm also learning new technologies and expanding my skill set to include the full-stack by learning MongoDB and NodeJS. My end goal is to become a full-stack and create fully functioning applications.                
+                <br /><br />
+                When I'm not coding, I read about psychology or philosophy, play or watch video games, and listen to music...</p>
                 <a href="/AbdellahBahsine_Resume.pdf" target="_blank"><button className={`${styles.cta} ${styles.primary}`}>Download Resume</button></a>
             </div>
         </section>
